897b1292637f2f262dc136c555f55d4d01fc7e22
Smartims <smartims@192.168.1.12>
2019-02-24 01:24:28 +0530

    Build Test

---
+++ ControlsDemo/LoginPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ControlsDemo
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        void LoginTapped(object sender, System.EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtUserName.Text))
+                DisplayAlert("Alert", "Please Enter UserName", "Ok");
+            else if (string.IsNullOrEmpty(txtPassword.Text))
+                DisplayAlert("Alert", "Please Enter Password", "Ok");
+            else
+                DisplayAlert("Alert", "userName" + " " + txtUserName.Text + " " + "txtPassword" + " " + txtPassword.Text, "Ok");
+            // throw new NotImplementedException();
+        }
+    }
+}
